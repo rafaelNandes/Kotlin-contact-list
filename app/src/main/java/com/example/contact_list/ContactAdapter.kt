aa8d795645170b69b9360d0c9b5b10fdf44399e7
rafaelNandes <rafael.fernandessilva@hotmail.com>
2021-05-27 22:36:32 -0300

    creating layout of the cardView and the functions from recyclerview

--- app/src/main/java/com/example/contact_list/ContactAdapter.kt
+++ app/src/main/java/com/example/contact_list/ContactAdapter.kt
@@ -1,28 +1,33 @@
 package com.example.contact_list
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
+//Gerenciamento de toda a lista do RecycleView
 class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHolder>(){
 
     private val list: MutableList<Contact> = mutableListOf()
 
+    //Responsável por criar a visualização de cada item na tela
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapterViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_item, parent, false)
+        return ContactAdapterViewHolder(view)
     }
 
+    //Percorre o array e popula a lista do RecycleView
     override fun onBindViewHolder(holder: ContactAdapterViewHolder, position: Int) {
         holder.bind(list[position])
     }
 
+    //Quantidade de itens que há na lista
     override fun getItemCount(): Int {
         return list.size
     }
 
+    //Gerencia cada item da lista
     class ContactAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-
-
         fun bind(contact: Contact){
 
         }
