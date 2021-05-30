dedadb9ca8dfa1db075e87b8226ba22e4b4a50a3
rafaelNandes <rafael.fernandessilva@hotmail.com>
2021-05-30 16:04:38 -0300

    adding option menu

--- app/src/main/java/com/example/contact_list/MainActivity.kt
+++ app/src/main/java/com/example/contact_list/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.contact_list
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -40,5 +44,29 @@ class MainActivity : AppCompatActivity() {
                 )
             )
         )
+    }
+
+    private fun showToast(message: String){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.itemMenu_1 -> {
+                showToast("Exibindo item de menu 1")
+                true
+            }
+            R.id.itemMenu_2 -> {
+                showToast("Exibindo item de menu 2")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
